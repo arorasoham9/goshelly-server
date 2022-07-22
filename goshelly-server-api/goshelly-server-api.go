9972a6eefcba94f2b7f0277946c6f7f8e668cc98
arorasoham9 <68672198+arorasoham9@users.noreply.github.com>
2022-07-21 20:44:16 -0400

    fds

--- goshelly-server-api/goshelly-server-api.go
+++ goshelly-server-api/goshelly-server-api.go
@@ -254,7 +254,7 @@ func createLink() {
 			return
 		}
 		link := ":"+PORT + "/logs/" + user.EMAIL + "/" + strconv.Itoa(user.LOGID) + "/" + token + "/"
-		c.JSON(http.StatusOK, gin.H{"message": link})
+		// c.JSON(http.StatusOK, gin.H{"message": link})
 		c.JSON(http.StatusOK, gin.H{"message": c.RemoteIP()+ c.ClientIP()})
 	})
 }
